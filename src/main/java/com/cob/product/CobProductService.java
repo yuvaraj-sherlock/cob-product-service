150fe5d9738d8e23b4756e2170a34736de15771c
Yuvaraj <yuvaraj.sf.java@gmail.com>
2025-05-20 11:57:50 +0530

    Initial project setup

---
+++ src/main/java/com/cob/product/CobProductService.java
@@ -0,0 +1,13 @@
+package com.cob.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobProductService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobProductService.class, args);
+	}
+
+}
